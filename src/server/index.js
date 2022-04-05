8a8772d704ff963f8613c940f85bfede97bae83f
Ryan <1ryanhart@gmail.com>
2022-04-05 09:37:00 +0100

    modified to allows CORS

--- src/server/index.js
+++ src/server/index.js
@@ -19,6 +19,7 @@ app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
+            .then(res.append('Access-Control-Allow-Origin', ['*']))
         res.send({ image })
     } catch (err) {
         console.log('error:', err);
